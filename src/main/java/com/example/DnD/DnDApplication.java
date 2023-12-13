c900a7b424789dfa72d51cfc57304acb489af281
PedroMagaieski <pedroalvesestevammagaieski@gmail.com>
2023-12-13 15:46:32 -0300

    Revert "exclude datasource"
    This reverts commit 68a0729f481130d71de35417ce3b16008420539a.

--- src/main/java/com/example/DnD/DnDApplication.java
+++ src/main/java/com/example/DnD/DnDApplication.java
@@ -2,11 +2,9 @@ package com.example.DnD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class DnDApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DnDApplication.class, args);
 	}
